6c0e2133c7a6e08d37ecc5cac263928fe08eb3e1
Samuel Bricker <94862877+sambricker216@users.noreply.github.com>
2022-05-12 13:04:59 -0400

    Added sample motion

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
  #Sim Window
 simWindow = pygame.Rect(50, 50, 400, 400)
 
+#Sample count
+SAMPLE_COUNT = 500
+
 #Color constants
 COLOR_GRAY = (125, 125, 125)
 COLOR_BLACK = (0,0,0)
@@ -36,10 +39,21 @@ class Sample:
     def move(self, newPOS):
         self.pos = newPOS
 
+def moveSample(sample):
+    range = randint(0,3)
+    if range == 0 and sample.pos[0] < 446: #Right
+        sample.move((sample.pos[0] + 2, sample.pos[1]))
+    elif range == 1 and sample.pos[0] > 53: #Left
+        sample.move((sample.pos[0] - 2, sample.pos[1]))
+    elif range == 2 and sample.pos[1] > 53: #Up
+        sample.move((sample.pos[0], sample.pos[1] - 2))
+    elif range == 3 and sample.pos[1] < 446: #Down
+        sample.move((sample.pos[0], sample.pos[1] + 2))
+
 def buildSamples(immunized):
     sampleMap = []
     usedLocs = {}
-    while len(sampleMap) < 100:
+    while len(sampleMap) < SAMPLE_COUNT:
         entered = False
         while entered == False:
             x = randint(50, 448)
@@ -65,6 +79,7 @@ def main():
     run = True
 
     samples = buildSamples(60)
+    mouse = [-1,-1]
 
     #Game Loop
     while(run):
@@ -73,16 +88,23 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mousePos = pygame.mouse.get_pos()
+                print(mousePos)
+                if 600 <= mousePos[0] <= 700 and 100 <= mousePos[1] <= 150:
+                    print("move")
+                    for sample in samples:
+                        moveSample(sample)
         
         sampleRect = []
-        for num in range(100):
+        for num in range(SAMPLE_COUNT):
             sampleRect.append(pygame.Rect(samples[num].pos[0], samples[num].pos[1], 2, 2))
 
         #Drawing to screen
         WINDOW.fill(COLOR_GRAY)
         pygame.Surface.fill(WINDOW, COLOR_BLACK, simWindow)
 
-        for num in range(100):
+        for num in range(SAMPLE_COUNT):
             if samples[num].stat == Status.INFECTED:
                pygame.Surface.fill(WINDOW, COLOR_RED, sampleRect[num])
             elif samples[num].stat == Status.IMMUNIZED:
@@ -90,7 +112,11 @@ def main():
             elif samples[num].stat == Status.COOL_DOWN:
                pygame.Surface.fill(WINDOW, COLOR_GREEN, sampleRect[num])
             else:
-               pygame.Surface.fill(WINDOW, COLOR_BLUE, sampleRect[num]) 
+               pygame.Surface.fill(WINDOW, COLOR_BLUE, sampleRect[num])
+        
+        #Move button
+        moveButton = pygame.Rect(600,100,100,50)
+        pygame.Surface.fill(WINDOW, COLOR_BLACK, moveButton)
 
         pygame.display.update()
     
