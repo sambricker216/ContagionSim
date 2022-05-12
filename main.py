1661682e4f7dbf01f5b5ad5a4745913c722b8a24
Samuel Bricker <94862877+sambricker216@users.noreply.github.com>
2022-05-11 21:09:09 -0400

    added sample building and visual

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from asyncio.windows_events import NULL
+from random import randint, random
 import pygame
 
 #Window dimensions
 WIDTH, HEIGHT = 900, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
+ #Sim Window
+simWindow = pygame.Rect(50, 50, 400, 400)
 
 #Color constants
 COLOR_GRAY = (125, 125, 125)
@@ -30,9 +35,30 @@ class Sample:
     def move(self, newPOS):
         self.pos = newPOS
 
+def buildSamples():
+    sampleMap = {}
+    while len(sampleMap) < 100:
+        entered = False
+        while entered == False:
+            x = randint(50, 448)
+            y = randint(50, 448)
+            try:
+                a = sampleMap.get((x,y)).pos
+                entered = False
+            except:
+                entered = True
+                sampleMap.update({(x,y): Sample(Status.CLEAN, (x,y))})
+    
+    return sampleMap
+
 #Main function
 def main():
     run = True
+
+    sampleList = buildSamples()
+    sampleRect = []
+    for pos in sampleList:
+        sampleRect.append(pygame.Rect(sampleList[pos].pos[0], sampleList[pos].pos[1], 2, 2))
 
     #Game Loop
     while(run):
@@ -42,12 +68,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
         
-        #Sim Window
-        simWindow = pygame.Rect(50, 50, 400, 400)
 
         #Drawing to screen
         WINDOW.fill(COLOR_GRAY)
         pygame.Surface.fill(WINDOW, COLOR_BLACK, simWindow)
+
+        for rect in sampleRect:
+            pygame.Surface.fill(WINDOW, COLOR_BLUE, rect)
 
         pygame.display.update()
 
